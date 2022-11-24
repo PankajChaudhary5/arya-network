41790128c98dc8d3416931b3cc8af6ea66b149f7
pankajchaudhary5 <pankajchaudhary172@gmail.com>
2022-11-24 23:27:13 +0530

    added supply to chain spec and added config for live chain

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -1,8 +1,10 @@
+use hex_literal::hex;
 use node_template_runtime::{
 	AccountId, AuraConfig, BalancesConfig, GenesisConfig, GrandpaConfig, Signature, SudoConfig,
 	SystemConfig, WASM_BINARY,
 };
 use sc_service::ChainType;
+use runtime_common::constants::{Balance, ARYA, TOKEN_DECIMALS};
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_core::{sr25519, Pair, Public};
 use sp_finality_grandpa::AuthorityId as GrandpaId;
@@ -36,6 +38,62 @@ pub fn authority_keys_from_seed(s: &str) -> (AuraId, GrandpaId) {
 	(get_from_seed::<AuraId>(s), get_from_seed::<GrandpaId>(s))
 }
 
+const TOKEN_SYMBOL: &str = "ARYA";
+const SS_58_FORMAT: u32 = 51;
+
+/// Total supply of token is 100_000_000.
+/// Initially we are distributing the total supply to the multiple accounts which is representing
+/// its category pool which we will update in later part of development.
+const SEED_SUPPLY: Balance = ARYA * 50_000_000;
+const INITIAL_SALE: Balance = ARYA * 50_000_000;
+
+/// Generate a default spec for the live chain (Prod).
+pub fn arya_config() -> Result<ChainSpec, String> {
+	let wasm_binary = WASM_BINARY.ok_or_else(|| "Live wasm not available".to_string())?;
+
+	Ok(ChainSpec::from_genesis(
+		// Name
+		"Arya Live",
+		// ID
+		"prod",
+		ChainType::Live,
+		move || {
+			testnet_genesis(
+				wasm_binary,
+				// Initial PoA authorities
+				vec![authority_keys_from_seed("Alice")],
+				// Sudo account
+				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				// Pre-funded accounts
+				vec![
+					(
+						hex!["88fd77d706e168d78713a6a927c1ddfae367b081fb2829b119bbcc6db9af401d"]
+							.into(),
+						SEED_SUPPLY,
+					),
+					(
+						hex!["04063fc1cbba917ced6c45091bf631de6a4db584dd55c1d67431661a5d57a575"]
+							.into(),
+						INITIAL_SALE,
+					),
+				],
+				true,
+			)
+		},
+		// Bootnodes
+		vec![],
+		// Telemetry
+		None,
+		// Protocol ID
+		None,
+		None,
+		// Properties
+		None,
+		// Extensions
+		None,
+	))
+}
+
 pub fn development_config() -> Result<ChainSpec, String> {
 	let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
 
@@ -54,10 +112,18 @@ pub fn development_config() -> Result<ChainSpec, String> {
 				get_account_id_from_seed::<sr25519::Public>("Alice"),
 				// Pre-funded accounts
 				vec![
-					get_account_id_from_seed::<sr25519::Public>("Alice"),
-					get_account_id_from_seed::<sr25519::Public>("Bob"),
-					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+					(get_account_id_from_seed::<sr25519::Public>("Alice"), 2000000),
+					(get_account_id_from_seed::<sr25519::Public>("Bob"), 1000000),
+					(get_account_id_from_seed::<sr25519::Public>("Charlie"), 1000000),
+					(get_account_id_from_seed::<sr25519::Public>("Dave"), 10000000),
+					(get_account_id_from_seed::<sr25519::Public>("Eve"), 1000000),
+					(get_account_id_from_seed::<sr25519::Public>("Ferdie"), 1000000),
+					(get_account_id_from_seed::<sr25519::Public>("Alice//stash"), 1000000),
+					(get_account_id_from_seed::<sr25519::Public>("Bob//stash"), 10000000),
+					(get_account_id_from_seed::<sr25519::Public>("Charlie//stash"), 1000000),
+					(get_account_id_from_seed::<sr25519::Public>("Dave//stash"), 1000),
+					(get_account_id_from_seed::<sr25519::Public>("Eve//stash"), 1000),
+					(get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"), 1000),
 				],
 				true,
 			)
@@ -94,18 +160,18 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 				get_account_id_from_seed::<sr25519::Public>("Alice"),
 				// Pre-funded accounts
 				vec![
-					get_account_id_from_seed::<sr25519::Public>("Alice"),
-					get_account_id_from_seed::<sr25519::Public>("Bob"),
-					get_account_id_from_seed::<sr25519::Public>("Charlie"),
-					get_account_id_from_seed::<sr25519::Public>("Dave"),
-					get_account_id_from_seed::<sr25519::Public>("Eve"),
-					get_account_id_from_seed::<sr25519::Public>("Ferdie"),
-					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
+					(get_account_id_from_seed::<sr25519::Public>("Alice"), 2000000),
+					(get_account_id_from_seed::<sr25519::Public>("Bob"), 1000000),
+					(get_account_id_from_seed::<sr25519::Public>("Charlie"), 1000000),
+					(get_account_id_from_seed::<sr25519::Public>("Dave"), 10000000),
+					(get_account_id_from_seed::<sr25519::Public>("Eve"), 1000000),
+					(get_account_id_from_seed::<sr25519::Public>("Ferdie"), 1000000),
+					(get_account_id_from_seed::<sr25519::Public>("Alice//stash"), 1000000),
+					(get_account_id_from_seed::<sr25519::Public>("Bob//stash"), 10000000),
+					(get_account_id_from_seed::<sr25519::Public>("Charlie//stash"), 1000000),
+					(get_account_id_from_seed::<sr25519::Public>("Dave//stash"), 1000),
+					(get_account_id_from_seed::<sr25519::Public>("Eve//stash"), 1000),
+					(get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"), 1000),
 				],
 				true,
 			)
@@ -129,7 +195,7 @@ fn testnet_genesis(
 	wasm_binary: &[u8],
 	initial_authorities: Vec<(AuraId, GrandpaId)>,
 	root_key: AccountId,
-	endowed_accounts: Vec<AccountId>,
+	endowed_accounts: Vec<(AccountId, Balance)>,
 	_enable_println: bool,
 ) -> GenesisConfig {
 	GenesisConfig {
@@ -139,7 +205,7 @@ fn testnet_genesis(
 		},
 		balances: BalancesConfig {
 			// Configure endowed accounts with initial balance of 1 << 60.
-			balances: endowed_accounts.iter().cloned().map(|k| (k, 1 << 60)).collect(),
+			balances: endowed_accounts,
 		},
 		aura: AuraConfig {
 			authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
